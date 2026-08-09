c15251a8b173ecbd035fae6edd52a2c38ea3d8fd
gominho-sbc <fgfatec@gmail.com>
2026-08-09 18:57:24 -0300

    Conclusão do desafio da trilha de fundamentos dotNet

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -6,11 +6,14 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
+//Código implementado para limpar a tela do console antes da inicialização
+Console.Clear();
+
+Console.Write($"Seja bem vindo ao sistema de estacionamento!\n" +
+                  "Digite o preço inicial: ");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine("Agora digite o preço por hora:");
+Console.Write($"Agora digite o preço por hora: ");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
@@ -23,11 +26,12 @@ bool exibirMenu = true;
 while (exibirMenu)
 {
     Console.Clear();
-    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("SISTEMA DE ESTACIONAMENTO DIO");
     Console.WriteLine("1 - Cadastrar veículo");
     Console.WriteLine("2 - Remover veículo");
     Console.WriteLine("3 - Listar veículos");
     Console.WriteLine("4 - Encerrar");
+    Console.Write($"Digite a sua opção: ");
 
     switch (Console.ReadLine())
     {
